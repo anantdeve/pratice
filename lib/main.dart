48f6c17d7c878ce1cc4f9a722c9728d0c69a0dfc
anantdeve <anantprajapati1026@gmail.com>
2026-05-19 14:35:51 +0530

    Update Text in Master Branch

--- lib/main.dart
+++ lib/main.dart
@@ -104,6 +104,13 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
+            const Text(
+              'Flutter master Branch',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
             const Text('You have pushed the button this many times:'),
             Text(
               '$_counter',
